ef87974dca9a921955ff88d7f0e1e9f4b8f84243
Lucas Schutz <lucas.schutz@aluno.fmpsc.edu.br>
2025-11-30 15:15:34 -0300

    Update ProdutoDAO.java

--- A3java/src/DAO/ProdutoDAO.java
+++ A3java/src/DAO/ProdutoDAO.java
@@ -16,13 +16,11 @@ public class ProdutoDAO {
         try {
             String driver = "com.mysql.cj.jdbc.Driver";
             Class.forName(driver);
-            
             String url = "jdbc:mysql://localhost:3306/estoqueprodutos?useTimezone=true&serverTimezone=UTC";
             String user = "root";
             String password = ""; 
 
             return DriverManager.getConnection(url, user, password);
-
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Erro de Conexão: " + e.getMessage());
             return null;
@@ -37,14 +35,13 @@ public class ProdutoDAO {
                 Statement stmt = conn.createStatement();
                 ResultSet res = stmt.executeQuery("SELECT * FROM estoque");
                 while (res.next()) {
-                    Produto p = new Produto(
+                    lista.add(new Produto(
                         res.getInt("id_produto"),
                         res.getString("nome_produto"),
                         res.getString("descricao_produto"),
                         res.getInt("quantidade_estoque"),
                         res.getDouble("preco")
-                    );
-                    lista.add(p);
+                    ));
                 }
                 stmt.close();
             }
@@ -56,23 +53,72 @@ public class ProdutoDAO {
 
     public boolean InsertProdutoBD(Produto objeto) {
         String sql = "INSERT INTO estoque(nome_produto, descricao_produto, quantidade_estoque, preco, data_cadastro) VALUES(?,?,?,?, CURDATE())";
-
         try {
             Connection conn = this.getConexao();
-            if (conn != null) {
-                PreparedStatement stmt = conn.prepareStatement(sql);
-                stmt.setString(1, objeto.getNome());
-                stmt.setString(2, objeto.getDescricao());
-                stmt.setInt(3, objeto.getQuantidade());
-                stmt.setDouble(4, objeto.getPreco());
-                
-                stmt.execute();
-                stmt.close();
-                return true;
-            }
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, objeto.getNome());
+            stmt.setString(2, objeto.getDescricao());
+            stmt.setInt(3, objeto.getQuantidade());
+            stmt.setDouble(4, objeto.getPreco());
+            stmt.execute();
+            stmt.close();
+            return true;
         } catch (SQLException erro) {
             JOptionPane.showMessageDialog(null, "Erro ao inserir: " + erro.getMessage());
+            return false;
         }
-        return false;
+    }
+
+    public boolean DeleteProdutoBD(int id) {
+        try {
+            Connection conn = this.getConexao();
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("DELETE FROM estoque WHERE id_produto = " + id);
+            stmt.close();
+            return true;
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao deletar: " + erro.getMessage());
+            return false;
+        }
+    }
+
+    public boolean UpdateProdutoBD(Produto objeto) {
+        String sql = "UPDATE estoque SET nome_produto=?, descricao_produto=?, quantidade_estoque=?, preco=? WHERE id_produto=?";
+        try {
+            Connection conn = this.getConexao();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, objeto.getNome());
+            stmt.setString(2, objeto.getDescricao());
+            stmt.setInt(3, objeto.getQuantidade());
+            stmt.setDouble(4, objeto.getPreco());
+            stmt.setInt(5, objeto.getId());
+            stmt.execute();
+            stmt.close();
+            return true;
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao atualizar: " + erro.getMessage());
+            return false;
+        }
+    }
+
+    public Produto carregaProduto(int id) {
+        Produto objeto = null;
+        try {
+            Connection conn = this.getConexao();
+            Statement stmt = conn.createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM estoque WHERE id_produto = " + id);
+            if (res.next()) {
+                objeto = new Produto(
+                    id,
+                    res.getString("nome_produto"),
+                    res.getString("descricao_produto"),
+                    res.getInt("quantidade_estoque"),
+                    res.getDouble("preco")
+                );
+            }
+            stmt.close();
+        } catch (SQLException erro) {
+        }
+        return objeto;
     }
 }
